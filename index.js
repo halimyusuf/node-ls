ce2a32b0be0fb217b875b0f34a2ce40982018b6a
halim <haleemyoosuph@gmail.com>
2020-06-26 16:06:33 +0100

    feat: accepts command line argument feat: shows total file size in bytes feat: shows numbers of files and folders in the specified directory

--- index.js
+++ index.js
@@ -2,27 +2,45 @@
 const fs = require('fs');
 const util = require('util');
 const chalk = require('chalk');
+const path = require('path');
+
+const targetDir = process.argv[2] || process.cwd();
 
 const lstat = util.promisify(fs.lstat);
-fs.readdir(process.cwd(), async (err, files) => {
+fs.readdir(targetDir, async (err, files) => {
   if (err) console.log(err);
+  //   console.log(files);
   else {
-    const statsPromise = files.map((file) => lstat(file));
+    const statsPromise = files.map((file) => lstat(path.join(targetDir, file)));
     const filesStats = await Promise.all(statsPromise);
+    let fileCount = 0;
+    let dirCount = 0;
+    let totalSize = 0;
     for (let stat of filesStats) {
       const i = filesStats.indexOf(stat);
       if (stat.isFile()) {
+        fileCount++;
         const size = `${stat.size}`;
+        totalSize += Number(size);
         console.log(
-          `${chalk.cyan(size)} ${' '.repeat(25 - size.length)} ${chalk.blue(
+          `  ${chalk.cyan(size)} ${' '.repeat(25 - size.length)} ${chalk.blue(
             files[i]
           )}`
         );
       } else {
+        dirCount++;
         console.log(
-          `${chalk.green('<DIR>')} ${' '.repeat(22)} ${chalk.yellow(files[i])}`
+          `  ${chalk.green('<DIR>')} ${' '.repeat(20)} ${chalk.yellow(
+            files[i]
+          )}`
         );
       }
     }
+    console.log(
+      `${' '.repeat(10)} ${fileCount} File(s) ${' '.repeat(
+        5
+      )} ${totalSize} bytes`
+    );
+    console.log(`${' '.repeat(10)} ${dirCount} Dir(s) `);
   }
 });
