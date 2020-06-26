e21f0c9300dea9d88b40b3630cb959d50c58ba6f
halim <haleemyoosuph@gmail.com>
2020-06-26 15:08:59 +0100

    chore: index.js works , node-ls now prints all files and folders in the current directory

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 #! /usr/bin/env node
+const fs = require('fs');
+const util = require('util');
 
-console.log('working');
+const lstat = util.promisify(fs.lstat);
+fs.readdir(process.cwd(), async (err, files) => {
+  if (err) console.log(err);
+  else {
+    for (let file of files) {
+      const stats = await lstat(file);
+      console.log(file, stats.isFile());
+    }
+  }
+});
