50d20ca21879304d0249b0706a5fb21859b5286e
halim <haleemyoosuph@gmail.com>
2020-06-26 16:22:18 +0100

    chore:  add commas to numbers

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const path = require('path');
 
 const targetDir = process.argv[2] || process.cwd();
 
+function numberWithCommas(x) {
+  return `${x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+}
+
 const lstat = util.promisify(fs.lstat);
 fs.readdir(targetDir, async (err, files) => {
   if (err) console.log(err);
@@ -23,9 +27,9 @@ fs.readdir(targetDir, async (err, files) => {
         const size = `${stat.size}`;
         totalSize += Number(size);
         console.log(
-          `  ${chalk.cyan(size)} ${' '.repeat(25 - size.length)} ${chalk.blue(
-            files[i]
-          )}`
+          `  ${chalk.cyan(numberWithCommas(size))} ${' '.repeat(
+            25 - size.length
+          )} ${chalk.blue(files[i])}`
         );
       } else {
         dirCount++;
@@ -39,7 +43,7 @@ fs.readdir(targetDir, async (err, files) => {
     console.log(
       `${' '.repeat(10)} ${fileCount} File(s) ${' '.repeat(
         5
-      )} ${totalSize} bytes`
+      )} ${numberWithCommas(totalSize)} bytes`
     );
     console.log(`${' '.repeat(10)} ${dirCount} Dir(s) `);
   }
